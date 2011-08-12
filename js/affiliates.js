87005d7e9430eb1dd3d3d1621b568e6ebce1126f
Juan Camilo Estela <juancamilo.estela@gmail.com>
2011-08-12 09:44:25 -0500

    month picker widget

--- js/affiliates.js
+++ js/affiliates.js
@@ -4,6 +4,7 @@
  */
 $(
 	function(event){
+		MonthPicker.init();
 		void HomePage.init();
 	}
 );
@@ -57,5 +58,139 @@ var HomePage = {
 		} else {
 			toolTip.hide();
 		}
+	}
+}
+
+
+
+
+
+/**
+ * Month Picker Class
+ */
+var MonthPicker = {
+
+	currentMonth:01,
+	currentYear:2011,
+	monthNames:['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+	opened:false,
+	monthList:null,
+	/**
+	 * Month Picker Initialization
+	 */
+	init: function(){
+		void MonthPicker.addEventListeners();
+	},
+	
+	/**
+	 *
+	 */
+	addEventListeners: function(){
+		var monthPicker = $('.month-year-picker');
+		if (monthPicker.length == 0){ return; }
+
+		// Cache Elements
+		MonthPicker.monthList = $('.month-picker');
+
+		// set current month
+		MonthPicker.currentMonth = parseInt($('.month-picker ul li.current-month a').attr('title')) || 01;
+
+		// set current year
+		MonthPicker.currentYear = parseInt($('.month-picker .year-picker .current-year').text()) || 2011;
+
+		// pull month name list from document
+		var months = $('.month-picker ul li a');
+		if (months.length > 0){
+			MonthPicker.monthNames = [];
+			$.each($('.month-picker ul li a'),function(i,val){
+				MonthPicker.monthNames.push($(this).html());
+			});
+		}
+
+		$('.picker-header .prev-month').click(function(e){
+			e.preventDefault();
+			MonthPicker.prevMonth();
+		});
+
+		$('.picker-header .current-month-year').click(function(e){
+			e.preventDefault();
+			if (MonthPicker.opened){
+				MonthPicker.close();
+			} else {
+				MonthPicker.open();
+			}
+		});
+
+		$('.picker-header .next-month').click(function(e){
+			e.preventDefault();
+			MonthPicker.nextMonth();
+		});
+
+		$('.year-picker .prev-year').click(function(e){
+			e.preventDefault();
+			MonthPicker.currentYear--;
+			MonthPicker.refresh();
+		});
+
+		$('.year-picker .next-year').click(function(e){
+			e.preventDefault();
+			MonthPicker.currentYear++;
+			MonthPicker.refresh();
+		});
+
+		$('.month-picker ul li a').click(function(e){
+			e.preventDefault();
+			MonthPicker.currentMonth = parseInt($(this).attr('title'),10);
+			MonthPicker.refresh();
+		});	
+
+	},
+	refresh:function(){
+		// set month-year
+		$('.month-year-picker .month-year').html(MonthPicker.monthNames[MonthPicker.currentMonth - 1] + ' ' + MonthPicker.currentYear);	
+
+		// set year
+		$('.month-picker .current-year').html(MonthPicker.currentYear);
+
+		// set month
+		$('.month-year-picker ul li').removeClass('current-month');
+		$.each($('.month-picker ul li'),function(i,val){
+			if ( (i+1) == MonthPicker.currentMonth){
+				$(this).addClass('current-month');
+			}
+		});
+		MonthPicker.getData();
+	},
+	open:function(){
+		MonthPicker.monthList.show();
+		MonthPicker.opened = true;
+	},
+	close:function(){
+		MonthPicker.monthList.hide();
+		MonthPicker.opened = false;
+	},
+	nextMonth:function(){
+		if (MonthPicker.currentMonth < 12){
+			MonthPicker.currentMonth++;
+		}else{
+			MonthPicker.currentMonth = 1;
+			MonthPicker.currentYear++;
+		}
+		MonthPicker.refresh();
+	},
+	prevMonth:function(){
+		if (MonthPicker.currentMonth > 1){
+			MonthPicker.currentMonth--;
+		}else{
+			MonthPicker.currentMonth = 12;
+			MonthPicker.currentYear--;
+		}
+		MonthPicker.refresh();
+	},
+	getData:function(){
+		// ajax call here
+		// Params 
+		// MonthPicker.currentMonth [int 1-12]
+		// MonthPicker.currentYear [int]
 	}
 }
